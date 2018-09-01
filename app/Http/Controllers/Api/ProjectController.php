bd02e2da9b2255d00058c614b1523bbe8cc618f8
kuromoka <kuromokapb@gmail.com>
2018-09-01 12:27:23 +0900

    Add api route

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return var_dump(rand());
+    }
+}
