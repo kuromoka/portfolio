1c6b26e41451b901da7851e4668889d233f3c5f8
kuromoka <kuromokapb@gmail.com>
2018-09-05 02:04:09 +0900

    Save inquiries table

--- app/Http/Controllers/Api/InquiryController.php
+++ app/Http/Controllers/Api/InquiryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Inquiry;
 
 class InquiryController extends Controller
 {
@@ -19,6 +20,12 @@ class InquiryController extends Controller
             'name' => 'required|max:255',
             'email' => 'required|email',
             'message' => 'required|max:1024',
+        ]);
+
+        Inquiry::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'message' => $request->message,
         ]);
     }
 }
--- app/Inquiry.php
+++ app/Inquiry.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inquiry extends Model
 {
-    //
+    protected $fillable = ['name', 'email', 'message'];
 }
