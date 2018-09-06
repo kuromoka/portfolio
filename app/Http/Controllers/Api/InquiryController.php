cd995206620dd947a8a1d4ac08515db2695d0e7a
kuromoka <kuromokapb@gmail.com>
2018-09-06 22:50:21 +0900

    Sending InquiryReceived mail

--- app/Http/Controllers/Api/InquiryController.php
+++ app/Http/Controllers/Api/InquiryController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Controller;
 use App\Inquiry;
+use App\Mail\InquiryReceived;
 
 class InquiryController extends Controller
 {
@@ -22,10 +24,12 @@ class InquiryController extends Controller
             'message' => 'required|max:1024',
         ]);
 
-        Inquiry::create([
+        $inquiry = Inquiry::create([
             'name' => $request->name,
             'email' => $request->email,
             'message' => $request->message,
         ]);
+
+        Mail::to($request->email)->send(new InquiryReceived($inquiry));
     }
 }
--- app/Mail/InquiryReceived.php
+++ app/Mail/InquiryReceived.php
@@ -6,19 +6,27 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Inquiry;
 
 class InquiryReceived extends Mailable
 {
     use Queueable, SerializesModels;
 
     /**
+     * Inquiry instance
+     *
+     * @var Inquiry
+     */
+    protected $order;
+
+    /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Inquiry $inquiry)
     {
-        //
+        $this->inquiry = $inquiry;
     }
 
     /**
@@ -28,6 +36,11 @@ class InquiryReceived extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.inquiries.received');
+        return $this->markdown('emails.inquiries.received')
+                    ->with([
+                        "name" => $this->inquiry->name,
+                        "email" => $this->inquiry->email,
+                        "message"  => $this->inquiry->message,
+                    ]);
     }
 }
