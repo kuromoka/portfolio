8f52c217865d8a61a1f0c5777787dce6d5d19c04
kuromoka <kuromokapb@gmail.com>
2018-09-11 21:47:37 +0900

    Add master blade file

---
+++ resources/views/portfolio/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>About kuromoka - @yield('title')</title>
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            @yield('content')
+        </div>
+        <script src=" {{ mix('js/app.js') }} "></script>
+    </body>
+</html>
--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Language Selection - About kuromoka</title>
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
-    </head>
-    <body>
-        <div id="app">
-            <app-component :is-index="true"></app-component>
-        </div>
-        <script src=" {{ mix('js/app.js') }} "></script>
-    </body>
-</html>
+@extends('portfolio.app')
+
+@section('title', 'Language Selection')
+
+@section('content')
+    <app-component :is-index="true"></app-component>
+@endsection
