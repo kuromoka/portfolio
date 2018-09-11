331d02fb079a393c66a75726dce416da648ab623
kuromoka <kuromokapb@gmail.com>
2018-09-11 21:58:56 +0900

    Add analyze tag

--- resources/views/portfolio/app.blade.php
+++ resources/views/portfolio/app.blade.php
@@ -8,8 +8,19 @@
         <title>About kuromoka - @yield('title')</title>
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TWDNCZR');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWDNCZR"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div id="app">
             @yield('content')
         </div>
